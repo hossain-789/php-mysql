002a85caa83964c9fb2450ae6cd6b0f30dcb0738
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-10-28 13:22:35 +0600

    first class

---
+++ php-mysql.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+$host="localhost";
+$username="root";
+$password=null;
+$databse="college";
+
+$conn = new mysqli($host, $username, $password, $databse);
+if($conn->connect_error){
+    die ("some error" .$conn->$connect_error);
+}
+echo "connection success";
+
+echo "<br/>";
+
+$result =  $conn->query("show tables")->fetch_all();
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+ */
+
+/* 
+ $host = "localhost";
+ $username = "root";
+ $password = null;
+ $databse = "college";
+
+ $conn = new mysqli($host, $username, $password, $databse);
+ if($conn->connect_error){
+    die ("some error" .$conn->connect_error);
+ }
+ echo "connection success";
+ echo "<br/>";
+
+ $result = $conn->query("show tables")->fetch_all();
+ echo "<pre>";
+ print_r($result);
+ echo "</pre>";
+*/
+
+/*
+$host = "localhost";
+$username = "root";
+$password = null;
+$databse = "college";
+
+$conn = new mysqli($host, $username, $password, $databse);
+if($conn->connect_error){
+    die ("some error" .$conn->$connect_error);
+}
+echo "connection error";
+echo "</br>";
+
+$result = $conn->query("show tables")->fetch_all();
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+*/
+
+/*
+$host = "localhost";
+$username = "root";
+$password = null;
+$databse = "college";
+
+$conn = new mysqli($host, $username, $password, $databse);
+if($conn->connect_error){
+    die ("some error" .$conn->$connect_error);
+}
+echo "connection succes";
+echo "<br/>";
+
+$result = $conn->query("show tables")->fetch_all();
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+*/
+
+$host = "localhost";
+$username = "root";
+$password = null;
+$databse = "college";
+
+$conn = new mysqli($host, $username, $password, $databse);
+if($conn->connect_error){
+    die ("some error" .$conn->$connect_error);
+}
+echo "connection success";
+
+echo "<br/>";
+
+$result = $conn->query("show tables")->fetch_all();
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+?>
