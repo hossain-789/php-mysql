6e80b232c3b5fd50152a2c1081837a7bec3039d5
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-10-28 22:39:28 +0600

    php pdo

---
+++ php-mysql-pdo.php
@@ -0,0 +1,112 @@
+<?php
+/*
+$host = "localhost";
+$username = "root";
+$password = null;
+
+
+try {
+    $conn = new PDO("mysql:host=$host; dbname=college", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connect done";
+} catch (PDOException $err) {
+    echo "connect failed $err->getMessage()";
+}
+*/
+/*
+try{
+    $conn = new PDO("mysql:host=$host;dbname=college", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connect success";
+} catch (PDOException $err){
+    echo "connect failed $err->getMessage()";
+}
+ */
+
+/*
+$host = "localhost";
+$username = "root";
+$password = null; 
+
+try{
+$conn = new PDO("mysql:host=$host;dbname=college", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+echo "connection success";
+
+} catch(PDOException $err){
+    echo "connect failed $err->getMessage()";
+}
+
+echo "<br/>";
+
+$result = $conn->query("show tables");
+while($row = $result->fetch(PDO::FETCH_NUM)){
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+} */
+
+/*
+$host = "localhost";
+$username = "root";
+$password = null;
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=college", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connect done";
+}catch(PDOException $err){
+    echo "connect faild $err->getMessage()";
+}
+
+echo "<br/>";
+
+$result = $conn->query("show tables");
+while($row = $result->fetch(PDO::FETCH_NUM)){
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+}
+*/
+/*
+$host = "localhost";
+$username = "root";
+$password = null;
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=college", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connection success";
+}catch(PDOException $err){
+    echo "connection failed $err->getMessage()";
+}
+
+echo "<br/>";
+
+$result = $conn->query("show tables");
+while($row = $result->fetch(PDO::FETCH_NUM)){
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+}
+*/
+
+$host = "localhost";
+$username = "root";
+$password = null;
+
+try{
+    $conn = new PDO("mysql:host=$host; dbname=college", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connection success";
+}catch(PDOException $err){
+    echo "connect failed .$err->getMessage()";
+}
+echo "<br/>";
+
+$result = $conn->query("show tables");
+while($row = $result->fetch(PDO::FETCH_NUM)){
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+}
--- php-mysql.php
+++ php-mysql.php
@@ -77,6 +77,7 @@ print_r($result);
 echo "</pre>";
 */
 
+/*
 $host = "localhost";
 $username = "root";
 $password = null;
@@ -94,4 +95,42 @@ $result = $conn->query("show tables")->fetch_all();
 echo "<pre>";
 print_r($result);
 echo "</pre>";
+*/
+
+/*
+$host = "localhost";
+$username = "root";
+$password = null;
+$databse = "college";
+
+$conn = new mysqli($host, $username, $password, $databse);
+if($conn->connect_error){
+    die ("some error" .$conn->$connect_error);
+}
+echo "Connection Success";
+echo "</br>";
+
+$result = $conn->query("show tables")-> fetch_all();
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+*/
+
+$host= "localhost";
+$username = "root";
+$password = null;
+$databse = "college";
+
+$conn = new mysqli($host, $username, $password, $databse);
+if($conn->connect_error){
+    die ("some error" .$conn->$connect_error);
+}
+echo "connection success";
+echo "<br/>";
+
+$result = $conn->query("show tables")->fetch_all();
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
 ?>
