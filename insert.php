d3ecc9533da822e66cadb54c77c984d740e53bea
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-11-14 16:09:01 +0600

    updated all file

--- insert.php
+++ insert.php
@@ -1,26 +1,24 @@
-
-
 <form action="./insert.php" method="post">
-<input type="text" name="name" placeholder="enter name">
-<br />
-<br />
-<input type="text" name="class" placeholder="enter class">
-<br />
-<br />
-<input type="text" name="marks" placeholder="enter marks">
-<br />
-<br />
-<input type="text" name="gender" placeholder="enter gender">
-<br />
-<br />
-<button type="submit">add new student</button>
+    <input type="text" name="name" placeholder="enter name">
+    <br />
+    <br />
+    <input type="text" name="class" placeholder="enter class">
+    <br />
+    <br />
+    <input type="text" name="marks" placeholder="enter marks">
+    <br />
+    <br />
+    <input type="text" name="gender" placeholder="enter gender">
+    <br />
+    <br />
+    <button type="submit">add new student</button>
 </form>
 
 <?php
 
 
 include("./config.php");
-if(isset($_POST['name'])){
+if (isset($_POST['name'])) {
     $name = $_POST['name'];
     $class = $_POST['class'];
     $marks = $_POST['marks'];
@@ -32,12 +30,12 @@ if(isset($_POST['name'])){
 INSERT INTO `student_result`(`name`, `class`, `marks`, `gender`)
 VALUES ('$name','$class','$marks','$gender')
 ");
-$students = $getStudents->execute();
-if($students){
-    echo "data inserted";
-}else {
-    echo "data not inserted";
-}
+    $students = $getStudents->execute();
+    if ($students) {
+        echo "data inserted";
+    } else {
+        echo "data not inserted";
+    }
 }
 
 
